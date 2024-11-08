6afc1ad8e91a937536675718e21565f0c5107dfc
Griffin <baxgriffin@gmail.com>
2024-11-08 13:02:56 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Header, Footer } from './containers';
+import {
+  Navbar,
+  Home,
+  About,
+  Chatbot,
+  Services,
+  Talktous,
+  Contactus,
+} from './components';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+  
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <Navbar />
+              <Home />
+              <Services />
+              <About />
+              <Chatbot />
+              <Contactus />
+              <Footer />
+            </>
+          }
+        />
+        
+        {/* Separate Route for Talk to Us */}
+        <Route
+          path="/talktous"
+          element={
+            <>
+              <Talktous />
+              <Footer />
+            </>
+          }
+        />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
